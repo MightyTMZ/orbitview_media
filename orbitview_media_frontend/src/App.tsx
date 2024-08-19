44cf02300dca538f31cfe20f2c6182a04fed6930
Tom Zhang <tommyawesome08@gmail.com>
2024-08-18 21:55:49 -0400

    added poppins font

--- orbitview_media_frontend/src/App.tsx
+++ orbitview_media_frontend/src/App.tsx
@@ -1,10 +1,27 @@
-
+import { HashRouter as Router, Route, Routes } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      
+      <Router>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet"
+      ></link>
+      <Routes>
+        {/*<Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<LandingPage />} />
+        <Route path="/portfolio/projects" element={<ProjectsRoutePage />} />
+        <Route path="/portfolio/projects/:id" element={<ProjectDetails />} />
+        <Route path="/certificates" element={<CertificatesPage />} />
+        <Route path="/connect-with-me" element={<ConnectWithMe />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/resume" element={<Resume />} />*/}
+      </Routes>
+    </Router>
     </>
   )
 }
