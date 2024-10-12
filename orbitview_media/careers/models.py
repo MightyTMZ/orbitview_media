ab2861e9ff4d29f9ae30a5a76abb730a3a406b21
Tom Zhang <tommyawesome08@gmail.com>
2024-10-11 23:08:48 -0400

    job application model created

--- orbitview_media/careers/models.py
+++ orbitview_media/careers/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class JobTag(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Job(models.Model):
+
+    EMPLOYMENT_TYPES = [
+        ('FT', 'Full-Time'), ('PT', 'Part-Time'), ('C', 'Contract')
+    ]
+
+    EXPERIENCE_REQUIRED = [('Entry', 'Entry-Level'), ('Mid', 'Mid-Level'), ('Senior', 'Senior-Level')]
+
+    title = models.CharField(max_length=255)
+    description = RichTextField()
+    responsibilities = RichTextField()
+    qualifications = RichTextField()
+    benefits = RichTextField()
+    employment_type = models.CharField(max_length=2, choices=EMPLOYMENT_TYPES)
+    salary_description = RichTextField() # Here we describe what the salary structure will be (e.g. minimum, maximum, piece-rate, etc.)
+    location = models.CharField()
+    is_active = models.BooleanField(default=True)
+    posted_on = models.DateTimeField(auto_now_add=True)
+    application_deadline = models.DateTimeField(null=True, blank=True)  # Deadline for applications
+    experience_level = models.CharField(max_length=50, choices=EXPERIENCE_REQUIRED, default='Entry')  # Experience level required
+    slug = models.SlugField(unique=True, max_length=255)  # SEO-friendly URLs for job postings
+    tags = models.ManyToManyField(JobTag, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.title} - {self.experience_level}"
+
+
+
+class JobApplication(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=2083)
+    linkedin_profile_url = models.CharField(max_length=2083)
+    additional_info = RichTextField(max_length=2083)
+    submitted_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.submitted_time}: {self.first_name} {self.last_name} applied for {self.job.title}"
